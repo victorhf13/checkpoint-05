5549da94427712c13d6eae5c0a8e9e5c9b61e4ac
Victor Henrique <victorhf1313@gmail.com>
2024-10-03 22:28:59 -0300

    subida projeto rodando compilado

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Grupo:
 Julia Menezes de Carvalho - 96106
+
 Victor Henrique Fernandes - 93598
+
 Willian Pereira Tedros - 87429
---
+++ app/src/main/java/br/com/fiap/checkpoint05/model/TicketResponse.kt
@@ -0,0 +1,15 @@
+package br.com.fiap.checkpoint05.model
+
+class TicketResponse(
+    val ticker: Ticker
+)
+
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
